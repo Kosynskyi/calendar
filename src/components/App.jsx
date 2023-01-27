5c382c8ddd8b5c5a54eaaf672a7bad39a2ed9e0b
Kosynskyi <kosynskyi.vova@gmail.com>
2023-01-27 19:05:51 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import Calendar from "./Calendar/Calendar";
 import Header from "./Header/Header";
 import { Box } from "./Box";
+import { objectCalendar } from "../data/objectCalendar";
+import isLeapYear from "date-fns/isLeapYear";
+import { selectCurrentYear } from "./redux/Date/selector";
 
 function App() {
+  const currentYear = useSelector(selectCurrentYear);
+  useEffect(() => {
+    isLeapYear(currentYear, objectCalendar);
+  }, [currentYear]);
+
   return (
     <Box p="40px" backgroundColor="#F8FDF8 " height="100vh">
       <Header />
